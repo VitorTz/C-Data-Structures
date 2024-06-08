ea88fe14f6d166772813dd44875bc092b3b49e01
VitorTz <vitor.fsz@proton.me>
2024-06-07 23:36:09 -0300

    Update

--- src/main.c
+++
@@ -1,8 +0,0 @@
-#include "tests.h"
-
-
-int main(int argc, char *argv[]) {    
-    srand(time(NULL));        
-    test_avl_tree_insert();
-    return 0;
-}
--- src/tests.h
+++
@@ -1,8 +0,0 @@
-#ifndef C_DATA_STRUCTURES_TESTS_H
-#define C_DATA_STRUCTURES_TESTS_H
-
-#include "test_array.h"
-#include "test_linked_list.h"
-#include "test_avl_tree.h"
-
-#endif
